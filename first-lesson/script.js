b9e265e2b4fce548592c3c7e00e513359ee999f6
Dustin Myers <dustinblaine.myers@gmail.com>
2020-05-13 12:20:19 -0400

    start lesson 5

--- first-lesson/script.js
+++ first-lesson/script.js
@@ -6,6 +6,8 @@ window.onload = function () {
   //Draw line steps
 
   context.beginPath(); // reset the context state
+  context.strokeStyle = "red"; //color of line
+  context.lineWidth = 10; //thickness of the line
   context.moveTo(30, 70); // moveTo(x,y) -> starting point of line
   context.lineTo(130, 70); // line(x,y) -> end point of the line
   context.stroke(); // draws the line
