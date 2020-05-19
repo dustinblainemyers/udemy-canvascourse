bdd6e34dc019c9c78a4d0d91cb802321d31d7b8d
Dustin Myers <dustinblaine.myers@gmail.com>
2020-05-19 13:29:30 -0400

    more practice making lines

--- line caps/canvas-script.js
+++ line caps/canvas-script.js
@@ -1,4 +1,13 @@
 window.onload = function () {
   var canvas = document.getElementById("line-caps-canvas");
   var context = canvas.getContext("2d");
+
+  //First line
+
+  context.beginPath();
+  context.strokeStyle = "red";
+  context.lineWidth = 10;
+  context.moveTo(100, 100);
+  context.lineTo(300, 100);
+  context.stroke();
 };
